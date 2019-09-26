47555ea2d0a3bbf9ef54ded130d07717e8c924ea
Michael Boly <bolym@oregonstate.edu>
2019-09-26 15:38:58 -0700

    templatized and video embedding working well, time to work on the adding a video feature.

--- public/videoTemplate.js
+++ public/videoTemplate.js
@@ -5,8 +5,8 @@ templates['video'] = template({"compiler":[7,">= 4.0.0"],"main":function(contain
 
   return "<div class=\"video\">\r\n  <h2>"
     + alias4(((helper = (helper = helpers.location || (depth0 != null ? depth0.location : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"location","hash":{},"data":data}) : helper)))
-    + "</h2>\r\n  <iframe "
+    + "</h2>\r\n  <iframe src=\""
     + alias4(((helper = (helper = helpers.embed || (depth0 != null ? depth0.embed : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"embed","hash":{},"data":data}) : helper)))
-    + " ></iframe>\r\n</div>\r\n";
+    + "\" width=\"1000\" height=\"624\" allow=\"autoplay; encrypted-media\" frameBorder=\"0\" allowfullscreen></iframe>\r\n</div>\r\n";
 },"useData":true});
 })();
